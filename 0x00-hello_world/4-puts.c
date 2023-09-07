3a636ea82322f5443fc328e2de5ca7d401aa8400
mohamed-hossam1000 <m01122528566@gmail.com>
2023-09-07 22:44:28 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 /**
-* Description
-*/
+ * op_add - Makes the sum of two numbers
+ * @arg1: First operand
+ * @arg2: Second operand
+ *
+ * Return: The sum of the two parameters
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
