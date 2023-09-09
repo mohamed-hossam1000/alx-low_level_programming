061d913b5811e8215f738d6b574acf3eb7e676c7
mohamed-hossam1000 <m01122528566@gmail.com>
2023-09-09 23:02:28 +0300

    q*q*q

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - j
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putvhar('0' + k);
+				if (i != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
