ae94840f01ea7d008962b6a81d325946c186a99e
mohamed-hossam1000 <m01122528566@gmail.com>
2023-09-09 22:47:46 +0300

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - j
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	for (i = 0; i < 6; i++)
+	{
+		putchar('a'+ i);
+	}
+	putchar('\n');
+	return (0);
+}
