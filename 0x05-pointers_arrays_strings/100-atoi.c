422efc9bb9ee4688df5204f6f34453aa94d248bc
mohamed-hossam1000 <m01122528566@gmail.com>
2023-09-18 21:21:12 +0300

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -22,7 +22,7 @@ int _atoi(char *s)
 	}
 	while (s[i] >= '0' && s[i] <= '9' && s[i] != '\0')
 	{
-		num = num * 10 * sign + (s[i] - '0') * sign;
+		num = num * 10 + (s[i] - '0') * sign;
 		i++;
 	}
 	return (num);
