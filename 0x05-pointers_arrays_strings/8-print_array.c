f92a1ca721706d79e32f9f6ea1cad3b533e17526
mohamed-hossam1000 <m01122528566@gmail.com>
2023-09-18 17:12:36 +0300

    arr

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+/**
+ * print_array - wtv
+ * @*a: arr
+ * @n: n
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if(i = n - 1)
+			printf(", ");
+		else
+			printf("\n");
+	}
+}
