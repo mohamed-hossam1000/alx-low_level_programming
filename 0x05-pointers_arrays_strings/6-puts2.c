9153c63ddc3854f319a1c5f547b9853e7d30eae1
mohamed-hossam1000 <m01122528566@gmail.com>
2023-09-18 16:34:54 +0300

    puts 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,15 @@
-void _putchar(char);
+4void _putchar(char);
 /**
  * puts2 - check the code
  * @str: 1
  */
 void puts2(char *str)
 {
-	_putchar(*str);
-	str += 2;
+	if (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
 	while (*str != '\0' && str[-1] != '\0')
 	{
 		_putchar(*str);
