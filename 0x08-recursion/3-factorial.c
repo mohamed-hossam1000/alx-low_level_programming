f1274a0cc09601fff2d7fb158de7fcce90c0b64f
mohamed-hossam1000 <m01122528566@gmail.com>
2023-09-30 23:58:15 +0300

    3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+/**
+ * factorial - check the code
+ * @n: n
+ *
+ * Return: n
+ */
+int factorial(int n)
+{
+	if (n == 1 || n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
