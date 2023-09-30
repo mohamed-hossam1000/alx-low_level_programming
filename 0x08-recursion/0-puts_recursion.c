7e69ecf746036d39a86588ad33aa60df44a32cbf
mohamed-hossam1000 <m01122528566@gmail.com>
2023-09-30 23:37:20 +0300

    wtv

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * _puts_recursion - wtv
+ * 
+ * @s : var
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+}
