1eb693ac6267d1dac926f8604803406bf80e8fc4
mohamed-hossam1000 <m01122528566@gmail.com>
2023-10-01 00:03:57 +0300

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+/**
+ * _pow_recursion- wtv
+ * @x: n1
+ * @y: n2
+ *
+ * Return: n3
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
