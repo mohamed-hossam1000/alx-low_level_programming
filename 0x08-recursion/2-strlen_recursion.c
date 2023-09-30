c1609af9a0aea3390a8ed77b8a83f85a62795ec5
mohamed-hossam1000 <m01122528566@gmail.com>
2023-09-30 23:54:44 +0300

    ez

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+void _putchar(char);
+/**
+ * _strlen_recursion- wtv
+ *
+ * @s : var
+ *
+ * Return: n
+ */
+int _strlen_recursion(char *s)
+{
+	if(*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		s++;
+		return (_strlen_recursion(s) + 1);
+	}
+}
