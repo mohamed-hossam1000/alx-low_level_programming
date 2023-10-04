8701833468b28e717bedb569658d54e3f5f0d191
mohamed-hossam1000 <m01122528566@gmail.com>
2023-10-05 01:27:52 +0300

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+/**
+ * _strdup- wtv
+ * @str: string
+ *
+ * Return: pointer
+ */
+char *_strdup(char *str);
+{
+	unsigned int i;
+	char *arr;
+
+	for (i = 0; str[i] != '\0'; i++);
+	arr = malloc(i + 1);
+	if (arr == NULL || i <= 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		arr[i] = str[i];
+	}
+	arr[i] = str[i];
+	return (arr);
+}
