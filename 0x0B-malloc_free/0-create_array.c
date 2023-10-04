f4cacbf870c7404041ab3ad13fcde589bfd9f2c0
mohamed-hossam1000 <m01122528566@gmail.com>
2023-10-05 01:10:02 +0300

    0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+/**
+ * create_array- wtv
+ * @size: n
+ * @c: n
+ *
+ * Retuen: pointer
+ * */
+char *create_array(unsigned int size, char c)
+{
+	int i, output;
+	char* arr = malloc(size);
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	if (size <= 0)
+		output = NULL;
+	else
+		output = arr;
+
+	return (output);
+}
