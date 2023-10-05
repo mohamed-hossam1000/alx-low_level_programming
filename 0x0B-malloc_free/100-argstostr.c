123602369f3b76712b85083c5cd9a990afb2969b
mohamed-hossam1000 <m01122528566@gmail.com>
2023-10-05 05:33:42 +0300

    100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+/**
+ * str_concat- wtv
+ * @ac: string
+ * @av:string
+ *
+ * Return: pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	unsigned int i, j, k = 0;
+	char *arr;
+
+	if (av == NULL || ac == 0)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+			for (j = 0; av[i][j] != '\0';)
+				j++;
+			k += j + 1;
+	}
+
+	arr = malloc(k);
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0, k = 0; i < ac; i++)
+	{
+			for (j = 0; av[i][j] != '\0'; j++)
+			{
+				arr[k] = av[i][j];
+				k++;
+			}
+			arr[k] = '\n';
+	}
+	arr[k] = '\0';
+	return (arr);
+}
