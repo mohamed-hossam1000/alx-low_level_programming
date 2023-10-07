ec9d87f5b837ccb81c17f25b51690e09a7ef1cf6
mohamed-hossam1000 <m01122528566@gmail.com>
2023-10-07 04:46:58 +0300

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,6 +9,11 @@
  */
 int main(__attribute__((unused))int argc, __attribute__((unused))char **argv)
 {
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
