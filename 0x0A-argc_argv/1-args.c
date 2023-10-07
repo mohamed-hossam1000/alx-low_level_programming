0ed18f9cfa260d06abccdc8ff21e9d44ab2e85d3
mohamed-hossam1000 <m01122528566@gmail.com>
2023-10-07 04:33:40 +0300

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- wtv
+ * @argc: 1
+ * @argv: 2
+ *
+ * Return: 0
+ */
+int main(__attribute__((unused))int argc,__attribute__((unused)) char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
