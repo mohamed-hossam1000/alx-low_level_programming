7696685124d43756ee7e2adf8fb7a072899ee5f0
mohamed-hossam1000 <m01122528566@gmail.com>
2023-10-10 00:49:57 +0300

    1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+/**
+ * string_nconcat- wtv
+ * @s1: s
+ * @s2: s
+ * @n: s
+ *
+ * Return: p
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ret;
+	int i = 0, j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[j] != '\0')
+		j++;
+	ret = malloc(j + n + 1);
+	for (i = 0; s1[i] != '\0'; i++)
+		ret[i] = s1[i];
+	for (i = 0; i < n && s2[i] != '\0'; i++)
+		ret[j + i] = s2[i];
+	ret[j + i] = '\0';
+
+	return (ret);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+ * malloc_checked- wtv
+ * @b: n
+ *
+ * Return: p
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ret;
+
+	ret = malloc(b);
+	if (ret == NULL)
+		exit(98);
+
+	return (ret);
+}
