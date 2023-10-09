bb668cacfb826ac4e205f9b25de48990ef3146f3
mohamed-hossam1000 <m01122528566@gmail.com>
2023-10-10 01:16:43 +0300

    3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+/**
+ * array_range- wtv
+ * @nmemb: min
+ * @size: max
+ *
+ * Return: p
+ */
+int *array_range(int min, int max)
+{
+	int *ret;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	ret = (int *)malloc((max - min + 1) * 4);
+	if (ret == NULL)
+		return (NULL);
+	for (i = 0; i <= max - min; i++)
+	{
+		ret[i] = min + i;
+	}
+
+	return (ret);
+}
