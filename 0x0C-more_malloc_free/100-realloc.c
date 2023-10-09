4feb45db9e109191a056d3e6c45c7aecee05c3db
mohamed-hossam1000 <m01122528566@gmail.com>
2023-10-10 01:35:46 +0300

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,24 +7,25 @@
  *
  * Return: p
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size) {
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
 	void *new_ptr;
 	int copy_size;
 
 	if (ptr == NULL)
 	{
-		return malloc(new_size);
+		return (malloc(new_size));
 	}
 
 	if (new_size == 0)
 	{
 		free(ptr);
-		return NULL;
+		return (NULL);
 	}
 
 	if (new_size == old_size)
 	{
-		return ptr;
+		return (ptr);
 	}
 
 	new_ptr = malloc(new_size);
