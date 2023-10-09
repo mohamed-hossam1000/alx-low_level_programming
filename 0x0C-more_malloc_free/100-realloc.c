3e48474d806627c5abbf99ed570ea3a4151550f2
mohamed-hossam1000 <m01122528566@gmail.com>
2023-10-10 01:38:57 +0300

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,9 +17,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (malloc(new_size));
 
 	if (new_size == 0)
+	{
 		free(ptr);
 		return (NULL);
-
+	}
 	if (new_size == old_size)
 		return (ptr);
 
