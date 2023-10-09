8f31b7000371f50e7b90e2b1e803a7a51745b718
mohamed-hossam1000 <m01122528566@gmail.com>
2023-10-10 00:26:55 +0300

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * malloc_checked- wtv
+ * @b: n
+ *
+ * Return: p
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ret = malloc(b);
+
+	if (ret == NULL)
+		exit(98);
+
+	return (ret);
+}
